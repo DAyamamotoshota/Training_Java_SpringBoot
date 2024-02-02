0ca028dfd5e6fc69274047306176d5a032b3e1ab
DAyamamotoshota <s_yamamoto@dreamarts.co.jp>
2024-02-02 10:29:27 +0900

    first commit

---
+++ src/main/java/com/sre/web_app_training/HelloController.java
@@ -0,0 +1,16 @@
+package com.sre.web_app_training;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello Hello");
+        return "hello";
+    }
+}
+
---
+++ src/test/java/com/sre/web_app_training/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sre.web_app_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
